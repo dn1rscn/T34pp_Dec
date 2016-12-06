365bf78c9a3c5830da9981312b865d05ca22fd57
Inigo21 <inigofraile@gmail.com>
2016-12-06 19:03:23 +0100

    inigo_tutoriales

---
+++ Assets/Scripts/04-Tutoriales/tutoriales_ejercicios.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tutoriales_ejercicios : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	//CERRAR LA PANTALLA DE TUTORIAL
+	public void CerrarPantallaTutorial()
+	{
+		GameObject.Find ("botonTutorial").SetActive (false);
+	}
+}
