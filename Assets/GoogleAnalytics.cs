f59842d83dfa5fcb8d76eab7e7216b5497399c19
JPalacio3D <jorge.jp92@gmail.com>
2017-04-19 21:15:47 +0200

    google analytics

---
+++ Assets/GoogleAnalytics.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoogleAnalytics : MonoBehaviour 
+{
+	public GoogleAnalyticsV4 Español;
+	//public GoogleAnalyticsV4 Ingles;
+	//public GoogleAnalyticsV4 Euskera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
