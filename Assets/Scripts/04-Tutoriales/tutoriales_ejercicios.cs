bc1fd89abcd285afda89192f5dbaf1663476745a
JPalacio3D <jorge.jp92@gmail.com>
2019-02-18 20:04:23 +0100

    tuto ejer

--- Assets/Scripts/04-Tutoriales/tutoriales_ejercicios.cs
+++ Assets/Scripts/04-Tutoriales/tutoriales_ejercicios.cs
@@ -1,22 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.iOS;
 
 public class tutoriales_ejercicios : MonoBehaviour 
 {
 	public GameObject botonTuto;
+    public GameObject botonTutoIpad;
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+        if (Application.loadedLevelName.Contains("1")|| Application.loadedLevelName.Contains("Canasta")|| Application.loadedLevelName.Contains("Dientes")|| Application.loadedLevelName.Contains("Emociones"))
+        {
+            if ((Device.generation.ToString()).Contains("iPad"))
+            {
+                botonTuto.SetActive(false);
+                botonTutoIpad.SetActive(true);
+            }
+            else
+            {
+                botonTuto.SetActive(true);
+                botonTutoIpad.SetActive(false);
+            }
+        }
+        else
+        {
+            CerrarPantallaTutorial();
+        }
+    }
 	
 	//CERRAR LA PANTALLA DE TUTORIAL
 	public void CerrarPantallaTutorial()
 	{
-		GameObject.Find ("botonTutorial").SetActive (false);
-	}
+        botonTuto.SetActive(false);
+        botonTutoIpad.SetActive(false);
+    }
 
 	public void info()
 	{
-		botonTuto.SetActive (true);
-	}
+        if ((Device.generation.ToString()).Contains("iPad"))
+        {
+            botonTuto.SetActive(false);
+            botonTutoIpad.SetActive(true);
+        }
+        else
+        {
+            botonTuto.SetActive(true);
+            botonTutoIpad.SetActive(false);
+        }
+    }
 }
